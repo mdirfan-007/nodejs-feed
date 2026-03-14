05e7361762b5896e76204c992a68cbec4b42a672
MD IRFAN <mdimrfat007@gmail.com>
2026-03-14 16:17:06 +0530

    updated frontend

--- Frontend/src/pages/CreatePost.jsx
+++ Frontend/src/pages/CreatePost.jsx
@@ -1,29 +1,43 @@
-import React from 'react'
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const CreatePost = () => {
-  const handleSubmit = (e) =>{
-    e.preventDefault()
-    const formData =  new FormData(e.target)
-    axios.post('http://localhost:3000/create-post',formData).then((res)=>{
-            console.log(res);
-
-    })
-      
-    
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    axios.post("http://localhost:3000/create-post", formData).then((res) => {
+      console.log(res);
+    });
+  };
   return (
-    <div className='bg-gray-700 flex h-screen items-center justify-center'>
-          <div className='flex flex-col gap-4 bg-gray-800 p-8 rounded-lg'>
-            <h1 className='text-3xl text-center font-bold text-white'>Create Post</h1>
-             <form onSubmit={handleSubmit} className='flex gap-4  flex-col' action="">
-                <input className='bg-white py-2 rounded-l px-3' name='image' type="file" accept='image/*' />
-                <input className='bg-white py-2 rounded-l px-3' type="text" placeholder='Enter caption...' />
-                <button className='bg-blue-500 text-white px-4 py-2 rounded'>Create</button>
-             </form>
-          </div>
-    </div>
-  )
-}
+    <div className="bg-white flex h-screen items-center justify-center">
+      <div className="flex flex-col gap-4 bg-white p-8 rounded-lg border border-gray-300">
+        <h1 className="text-3xl text-center font-bold text-black">
+          Create Post
+        </h1>
+        <form
+          onSubmit={handleSubmit}
+          className="flex gap-4  flex-col"
+          action=""
+        >
+          <p htmlFor="file-1">
+            Upload image <span className="text-destructive">*</span>
+          </p>
+          <Input type="file" placeholder="Enter text" accept="image/*" />
+          <p htmlFor="file-1">
+            Post <span className="text-destructive">*</span>
+          </p>
+          <Input placeholder="Enter text" />
 
-export default CreatePost
+          <Button className={"text-white"} type="submit">
+            Create
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePost;
--- Frontend/src/pages/Feed.jsx
+++ Frontend/src/pages/Feed.jsx
@@ -24,10 +24,10 @@ const Feed = () => {
   
 
   return (
-    <div className="bg-gray-200 h-screen flex flex-col items-center justify-center">
+    <div className="bg-white h-screen flex flex-col items-center justify-center">
       {posts.map((posts) => (
-        <div className="h-[20rem] w-[14rem] p-3 mt-2 rounded-2xl  bg-white" key={posts.id}>
-          <img className=" mb-3 rounded-xl" src={posts.image} alt={posts.caption} />
+        <div className="h-[40rem] w-[24rem] p-3  mt-2 rounded-2xl  border border-gray-300 bg-white" key={posts.id}>
+          <img className=" mb-3 w-[50%] rounded-xl" src={posts.image} alt={posts.caption} />
           <h2 className="text-sm font-semibold">{posts.caption}</h2>
         </div>
       ))}
